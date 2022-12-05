d11c97586db99842790d2b356cb2f658cc3f6429
shengo333 <bekashengelia333@gmail.com>
2022-12-05 10:19:28 +0400

    junk again

--- first-app.js
+++ first-app.js
@@ -1,61 +1,17 @@
-const person = {
-    name: "Max",
-    age: 35,
-    greeting(){
-        console.log('hey there ' + this.name)
-    }
+const fetchData = () => {
+    const promise = new Promise((resolve,reject)=>{
+        setTimeout(()=> {
+            resolve('Done')
+        }, 5000)
+    });
+    return promise
 }
 
-const printName = ({name, age}) => {
-    console.log(name, age)
-}
-
-
-printName(person)
-
-const {name, age} = person 
-
-console.log(name, age)
-
-const hobbies = ['sport', 'gaming', 'running'];
-
-const [hobby1, hobby2] = hobbies;
-
-console.log(hobby2, hobby1)
-
-// for (let hoby of hobbies){
-
-//     console.log(hoby)
-// }
-
-
-// hobbies.forEach(e =>{
-//     console.log(e)
-// })
-
-// console.log(hobbies.map(hobby => 'Hobby: ' + hobby))
-
-
-
-// const result = hobbies.filter((element, index) => {
-//     return index != 2
-// })
-
-// console.log(result)
-
-// const copiedArray = [...hobbies]
-
-// console.log(copiedArray, 'this is copy')
-
-
-// const copiedPerson = {...person}
-
-// console.log(copiedPerson, 'copied person')
-
-
-// const toArray = (...args) => {
-//     return args
-// }
-
-// console.log(toArray(1,2,3,10,18))
-
+setTimeout(() =>{
+    fetchData().then(text =>{
+        console.log(text)
+        return fetchData()
+    }).then(text2 =>{
+        console.log(text2)
+    })
+}, 2000)
