747e3bc07e8cd3389666ca66535d3f31d23bea7c
shengo333 <bekashengelia333@gmail.com>
2022-12-06 11:21:04 +0400

    request

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 
 const server = http.createServer((req,res)=>{
-    console.log(req)
-    console.log(res)
+    console.log(req.url, req.method, req.headers);
+    // console.log(res);
+    process.exit();
 });
 
 server.listen(3000);
