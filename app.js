98a6690fe144bd175865de8e48dca72b7b24f84f
shengo333 <bekashengelia333@gmail.com>
2022-12-07 16:52:55 +0400

    some more rs

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    // console.log(res);
-    // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'Dummy text');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
