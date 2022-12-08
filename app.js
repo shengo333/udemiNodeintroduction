92bb17862e8ea7cc9c9d2737e2362e1154bfecd3
shengo333 <bekashengelia333@gmail.com>
2022-12-08 09:46:26 +0400

    testing

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ const server = http.createServer((req,res)=>{
         return res.end();
     }
     if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody)
+        })
         fs.writeFileSync('message.txt', 'Dummy text');
         res.statusCode = 302;
         res.setHeader('Location', '/');
