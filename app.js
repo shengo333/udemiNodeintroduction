7e50661fe0ecb408fa9406c25e7cea768a8ef552
shengo333 <bekashengelia333@gmail.com>
2023-01-23 12:31:48 +0400

    error added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 
 const app = express();
 
@@ -12,7 +15,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    console.log(__dirname, 'this is dirname i guess')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
 app.listen(3000);
